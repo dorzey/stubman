fe0e7f0ce191aa1b039300d76489715d5e4c2aaa
Paul Doran <dorzey@gmail.com>
2014-07-02 07:40:16 +0100

    Start of tests for main.js

---
+++ test/stubman/cli/main_test.js
@@ -0,0 +1,39 @@
+/*
+ * stubman
+ * https://github.com/dorzey/stubman
+ *
+ * Copyright (c) 2014 Paul Doran
+ * Licensed under the MIT license.
+ */
+
+var Main = require('../../../lib/stubman').Cli.Main();
+
+var mockResponse = function(){
+    var self = {};
+
+    self.headers = [];
+
+    self.set = function(k,v){
+        var header = {};
+        header[k] = v;
+        self.headers.push(header);
+    }
+
+    return self;
+};
+
+exports.group = {
+    test_setHeaders: function(test){
+        var res = mockResponse();
+        var headerOne = {key: 'Content-Type', value: 'application/json'};
+        var headerTwo = {key: 'Connection', value: 'keep-alive'};
+        var expectedheaders = [headerOne, headerTwo];
+
+        Main._setHeaders(res, expectedheaders);
+
+        test.equal(res.headers.length, 2);
+        test.equal(res.headers[0]['Content-Type'], headerOne.value);
+        test.equal(res.headers[1]['Connection'], headerTwo.value);
+        test.done();
+    }
+}
